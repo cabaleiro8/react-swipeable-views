2660a5e784f35e0bb3802cb96c1fc07f01638ba9
Iván Cabaleiro <i.cabaleiro@devcups.com>
2019-10-11 13:09:30 +0100

    Adding new consoleog

--- packages/react-swipeable-views/src/SwipeableViews.js
+++ packages/react-swipeable-views/src/SwipeableViews.js
@@ -404,6 +404,7 @@ class SwipeableViews extends React.Component {
   handleSwipeMove = event => {
     // The touch start event can be cancel.
     // Makes sure we set a starting point.
+    console.log('THE REACTSWIPEABLEVIEWS COMPONENT is handleswipemove!! YEAAAAH!');
     if (!this.started) {
       this.handleTouchStart(event);
       return;
